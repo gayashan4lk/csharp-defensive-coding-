189eb1a3f759a0a1536877acaa2a54418769804e
Gayashan Wagachchi <gayashan4lk@gmail.com>
2023-01-20 01:01:47 +0530

    add product service

---
+++ ACME.BL/Services/ProductService.cs
@@ -0,0 +1,18 @@
+﻿using ACME.BL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACME.BL.Services
+{
+    internal class ProductService
+    {
+        public static decimal calculateMargin(Product product)
+        {
+            decimal margin = (product.Price - product.Cost) / product.Price * 100M;
+            return margin;
+        }
+    }
+}
