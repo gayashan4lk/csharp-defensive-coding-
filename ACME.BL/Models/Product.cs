adaa280fd3a709e9c97dad0126d0a1d8943ca90d
Gayashan Wagachchi <gayashan4lk@gmail.com>
2023-01-20 01:00:34 +0530

    add product model

---
+++ ACME.BL/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACME.BL.Models
+{
+    internal class Product
+    {
+        public int ProductId { get; set; }
+        public decimal Price { get; set; }
+        public decimal Cost { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public Product(decimal price, decimal cost)
+        {
+            ProductId = ++this.ProductId;
+            Price = price;
+            Cost = cost;
+            CreatedDate = DateTime.Now;
+        }
+    }
+}
