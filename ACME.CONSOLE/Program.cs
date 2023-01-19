e13d1c7cb733ecef446a2cdab886af71380b8275
Gayashan Wagachchi <gayashan4lk@gmail.com>
2023-01-20 01:08:15 +0530

    add console main method

--- ACME.CONSOLE/Program.cs
+++ ACME.CONSOLE/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ACME.BL.Models;
+using ACME.BL.Services;
+
+Console.WriteLine("Programe Starting...");
+Console.WriteLine("Enter the Price:");
+var price = Console.ReadLine();
+Console.WriteLine("Enter the Cost:");
+var cost = Console.ReadLine();
+
+if (!string.IsNullOrEmpty(price) && !string.IsNullOrEmpty(cost))
+{
+    Console.WriteLine("Calculating product margine...");
+    var myProduct = new Product(Decimal.Parse(price), Decimal.Parse(cost));
+    var margin = ProductService.calculateMargin(myProduct);
+    Console.WriteLine(margin);
+}
+
+Console.WriteLine("\nPress any key to exit.");
+Console.ReadLine();
+
